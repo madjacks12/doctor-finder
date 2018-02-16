c064d4eb700158a513f1cc5d8e42e635bcb1f7aa
MJ <mdjacksonvideo@gmail.com>
2018-02-16 14:53:32 -0800

    api call correctly returns address and image of doctor

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -11,14 +11,19 @@ $('#form').submit(function (event) {
 
   apiCall.then(function (response) {
   		let body = JSON.parse(response);
-      console.log(body.data);
       if (body.data.length == 0) {
         $('#results').append("<p>No doctors with that name are currently accepting new patients in Portland, OR for that condition</p>");
       };
       for (let i = 0; i < body.data.length; i++) {
-        $('#results').append(body.data[i].profile.last_name + ", " + body.data[i].profile.first_name + "<br>" + body.data[i].practices.visit_address);
-        } if (body.data[i].practices.accepts_new_patients = true) {
-          $('#results').append("<p> - Accepts New Patients: Yes</p>")
+        console.log(body.data[i]);
+        $('#results').append(body.data[i].profile.last_name + ", " + body.data[i].profile.first_name + "<br>"
+        body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.state_long + " " + body.data[i].practices[0].visit_address.zip + "<br>" +
+        "<img src=" +body.data[i].profile.image_url + ">");
+        if (body.data[i].practices.accepts_new_patients = true) {
+          $('#results').append("<p> - Accepting New Patients: Yes</p>")
+      } else if (body.data[i].practices.accepts_new_patients = false) {
+          $('#results').append("<p> - Accepting New Patients: No</p>")
+      }
       }
       }, function (error) {
   		$('.showErrors').text(`There was an error processing your request: ${error.message}`);
